1289d9da6e450c5a313b873e5f932f0d848c51b3
Chintan Shah <shah.c@husky.neu.edu>
2018-10-12 11:35:24 -0400

    Update UsersController.java
    Json response

--- webapp/src/main/java/com/csye6225/controller/UsersController.java
+++ webapp/src/main/java/com/csye6225/controller/UsersController.java
@@ -1,5 +1,6 @@
 package com.csye6225.controller;
 
+import com.csye6225.model.Response;
 import com.csye6225.model.Users;
 import com.csye6225.repository.UserJpaRespository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Base64;
@@ -22,56 +24,68 @@ public class UsersController {
     @Autowired
     private UserJpaRespository userJpaRespository;
 
+    private String response = null;
+
     @PostMapping(value = "/register",produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Map<String, String> register(@RequestBody Users user) {
+    public void register(@RequestBody Users user, HttpServletResponse response) {
         try {
+            response.setContentType("application/json");
             List<Users> loginusers = userJpaRespository.findAll();
             for (Users l: loginusers) {
                 if (l.getUsername().equals(user.getUsername()))
                 {
-                    return Collections.singletonMap("response", "User already present");
+                    this.response = Response.jsonString("User already present");
+                    response.getWriter().write(this.response);
+                    return;
 
                 }
             }
             String pw_hash = BCrypt.hashpw(user.getPwd(), BCrypt.gensalt());
             user.setPwd(pw_hash);
             userJpaRespository.save(user);
-
-            return Collections.singletonMap("response", "You are successfully logged in");
+            this.response = Response.jsonString("User created");
+            response.getWriter().write(this.response);
         }
         catch (Exception ex)
         {
             System.out.println("Exception caught while register user : " +ex.getMessage());
-            return Collections.singletonMap("response", "Exception occurred..Pls try after sometime");
         }
 
     }
     @GetMapping(value = {"/","/time"},produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Map<String, String> gettime(HttpServletRequest httpRequest) {
-        final String authorization = httpRequest.getHeader("Authorization");
-        if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
-            // Authorization: Basic base64credentials
-            String base64Credentials = authorization.substring("Basic".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-            // credentials = username:password
-            final String[] values = credentials.split(":", 2);
-            String username = values[0];
-            String pwd = values[1];
-            List<Users> userlist = userJpaRespository.findAll();
-            for (Users u : userlist) {
-                if (u.getUsername().equals(username)) {
-                    if(BCrypt.checkpw(pwd, u.getPwd()))
-                    {
-                        return Collections.singletonMap("response", LocalDateTime.now().toString());
+    public void gettime(HttpServletRequest httpRequest, HttpServletResponse response) {
+        try {
+            response.setContentType("application/json");
+            final String authorization = httpRequest.getHeader("Authorization");
+            if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
+                // Authorization: Basic base64credentials
+                String base64Credentials = authorization.substring("Basic".length()).trim();
+                byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+                String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+                // credentials = username:password
+                final String[] values = credentials.split(":", 2);
+                String username = values[0];
+                String pwd = values[1];
+                List<Users> userlist = userJpaRespository.findAll();
+                for (Users u : userlist) {
+                    if (u.getUsername().equals(username)) {
+                        if (BCrypt.checkpw(pwd, u.getPwd())) {
+                            this.response = Response.jsonString(LocalDateTime.now().toString());
+                            response.getWriter().write(this.response);
+                        }
                     }
                 }
+                this.response = Response.jsonString("Pls Login");
+                response.getWriter().write(this.response);
+            } else {
+                this.response = Response.jsonString("Pls Login");
+                response.getWriter().write(this.response);
             }
-            return Collections.singletonMap("response", "You are not logged in..");
-        } else {
-            return Collections.singletonMap("response", "You are not logged in..");
+        }
+        catch(Exception ex) {
+
         }
     }
 
