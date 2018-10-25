36f304f54b488a906ea8b0977fb409c3f11fae5f
Sayali Gaikawad <43121282+sayaligaikawad@users.noreply.github.com>
2018-10-24 23:33:46 -0400

    Update Runner.java

--- webapp/src/main/java/com/csye6225/Runner.java
+++ webapp/src/main/java/com/csye6225/Runner.java
@@ -1,10 +1,11 @@
 package com.csye6225;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class Runner {
+public class Runner extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		
